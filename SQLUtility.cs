9d82aea461a0dfd3488522a38c4caed50118f61b
RS-codesandprograms <codesandprograms@gmail.com>
2025-03-16 17:32:42 -0500

    Session 22

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -41,12 +41,13 @@ namespace CPUFramework
                 try
                 {
                     SqlDataReader dr = cmd.ExecuteReader();
+                    CheckReturnValue(cmd);
                     if (loadtable == true)
                     {
                         dt.Load(dr);
                     }
                 }
-                catch(SqlException ex)
+                catch (SqlException ex)
                 {
                     string msg = ParseConstraintMsg(ex.Message);
                     throw new Exception(msg);
@@ -55,11 +56,48 @@ namespace CPUFramework
                 {
                     throw new Exception(cmd.CommandText + ": " + ex.Message, ex);
                 }
-                
+
             }
             SetAllColumnsAllowNull(dt);
             return dt;
         }
+
+
+        private static void CheckReturnValue(SqlCommand cmd)
+        {
+            int returnvalue = 0;
+            string msg = "";
+            if (cmd.CommandType == CommandType.StoredProcedure)
+            {
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction == ParameterDirection.ReturnValue)
+                    {
+                        if (p.Value != null)
+                        {
+                            returnvalue = (int)p.Value;
+                        }
+                    }
+                    else if (p.ParameterName.ToLower() == "@message")
+                    {
+                        if (p.Value != null)
+                        {
+                            msg = p.Value.ToString();
+                        }
+                    }
+                }
+                if (returnvalue == 1)
+                    {
+                        if (msg == "")
+                        {
+                            msg = $"{cmd.CommandText} did not do requested action.";
+                        }
+                        throw new Exception(msg);
+                    }
+                
+            }
+        }
+
 
         public static DataTable GetDataTable(string sqlstatement) // take a SQL statement and return a DataTable
         {
@@ -82,7 +120,7 @@ namespace CPUFramework
             {
                 cmd.Parameters[paramname].Value = value;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception(cmd.CommandText + ": " + ex.Message, ex);
             }
@@ -97,10 +135,10 @@ namespace CPUFramework
             string origmsg = msg;
             string prefix = "ck_";
             string msgend = "";
-            if (msg.Contains(prefix)== false)
-            { 
+            if (msg.Contains(prefix) == false)
+            {
                 if (msg.Contains("u_"))
-                { 
+                {
                     prefix = "u_";
                     msgend = " must be unique.";
                 }
@@ -129,97 +167,97 @@ namespace CPUFramework
                     if (prefix == "f_")
                     {
                         var words = msg.Split(" ");
-                        if(words.Length > 1)
+                        if (words.Length > 1)
                         {
                             msg = $"Cannot delete {words[0]} because it has a related {words[1]} record.";
                         }
                     }
                 }
-               
+
             }
             return msg;
 
         }
 
 
-            public static int GetFirstColumnFirstRowValue(string sql)
+        public static int GetFirstColumnFirstRowValue(string sql)
+        {
+            int n = 0;
+            DataTable dt = GetDataTable(sql);
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
             {
-                int n = 0;
-                DataTable dt = GetDataTable(sql);
-                if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
+                if (dt.Rows[0][0] != DBNull.Value)
                 {
-                    if (dt.Rows[0][0] != DBNull.Value)
-                    {
-                        int.TryParse(dt.Rows[0][0].ToString(), out n);
-                    }
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
                 }
-                return n;
+            }
+            return n;
+        }
+
+
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+            foreach (DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
+        }
+
+        public static string GetSQL(SqlCommand cmd)
+        {
+            string val = "";
+#if DEBUG
+            StringBuilder sb = new StringBuilder();
+            if (cmd.Connection != null)
+            {
+                sb.AppendLine($"--{cmd.Connection.DataSource}");
+                sb.AppendLine($"use {cmd.Connection.Database}");
+                sb.AppendLine("go");
+            }
+            if (cmd.CommandType == CommandType.StoredProcedure)
+            {
+                sb.AppendLine($"exec {cmd.CommandText}");
+                int paramcount = cmd.Parameters.Count - 1;
+                int paramnum = 0;
+                string comma = ",";
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction != ParameterDirection.ReturnValue)
+                    {
+                        if (paramnum == paramcount)
+                        {
+                            comma = "";
+                        }
+                        sb.AppendLine($"{p.ParameterName} = {(p.Value == null ? "null" : p.Value.ToString())}{comma}");
+
+                    }
+                    paramnum++;
+                }
+            }
+            else
+            {
+                sb.AppendLine($"{cmd.CommandText}");
             }
 
 
-            private static void SetAllColumnsAllowNull(DataTable dt)
+            val = sb.ToString();
+#endif
+            return val;
+        }
+        public static void DebugPrintDataTable(DataTable dt)
+        {
+            foreach (DataRow r in dt.Rows)
             {
                 foreach (DataColumn c in dt.Columns)
                 {
-                    c.AllowDBNull = true;
+                    Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
                 }
             }
-
-            public static string GetSQL(SqlCommand cmd)
-            {
-                string val = "";
-#if DEBUG
-                StringBuilder sb = new StringBuilder();
-                if (cmd.Connection != null)
-                {
-                    sb.AppendLine($"--{cmd.Connection.DataSource}");
-                    sb.AppendLine($"use {cmd.Connection.Database}");
-                    sb.AppendLine("go");
-                }
-                if (cmd.CommandType == CommandType.StoredProcedure)
-                {
-                    sb.AppendLine($"exec {cmd.CommandText}");
-                    int paramcount = cmd.Parameters.Count - 1;
-                    int paramnum = 0;
-                    string comma = ",";
-                    foreach (SqlParameter p in cmd.Parameters)
-                    {
-                        if (p.Direction != ParameterDirection.ReturnValue)
-                        {
-                            if (paramnum == paramcount)
-                            {
-                                comma = "";
-                            }
-                            sb.AppendLine($"{p.ParameterName} = {(p.Value == null ? "null" : p.Value.ToString())}{comma}");
-
-                        }
-                        paramnum++;
-                    }
-                }
-                else
-                {
-                    sb.AppendLine($"{cmd.CommandText}");
-                }
-
-
-                val = sb.ToString();
-#endif
-                return val;
-            }
-            public static void DebugPrintDataTable(DataTable dt)
-            {
-                foreach (DataRow r in dt.Rows)
-                {
-                    foreach (DataColumn c in dt.Columns)
-                    {
-                        Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
-                    }
-                }
-            }
-
-
-
-
         }
+
+
+
+
     }
+}
 
